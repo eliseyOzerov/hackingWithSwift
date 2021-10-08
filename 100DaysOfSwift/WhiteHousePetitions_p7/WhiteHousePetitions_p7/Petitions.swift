9ea0e54ae33f327d4ac00cdfb4b8318e39e16458
Elisey <eliseyozerov@outlook.com>
2021-10-08 19:27:31 +0200

    WhiteHousePetitions_p7

---
+++ 100DaysOfSwift/WhiteHousePetitions_p7/WhiteHousePetitions_p7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  WhiteHousePetitions_p7
+//
+//  Created by Elisey Ozerov on 08/10/2021.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
