b6605ffa4c773e8951a85274ae912e0bb90503ca
Elisey <eliseyozerov@outlook.com>
2021-10-12 19:21:13 +0200

    NamesToFaces_p10

---
+++ 100DaysOfSwift/StormViewer_p1 2/StormViewer/StormCell.swift
@@ -0,0 +1,14 @@
+//
+//  StormCellCollectionViewCell.swift
+//  StormViewer
+//
+//  Created by Elisey Ozerov on 12/10/2021.
+//
+
+import UIKit
+
+class StormCell: UICollectionViewCell {
+    
+    @IBOutlet var title: UILabel!
+    @IBOutlet var image: UIImageView!
+}
