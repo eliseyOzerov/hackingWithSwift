1ac6028b62a91f2258242a025a8e7efdb01a7a2c
Elisey <eliseyozerov@outlook.com>
2021-11-24 15:47:14 +0100

    SwiftStrings_c9

--- 100DaysOfSwift/SwiftStrings_p24.playground/Contents.swift
+++ 100DaysOfSwift/SwiftStrings_p24.playground/Contents.swift
@@ -13,3 +13,37 @@ extension String {
 }
 
 let letter2 = name[3]
+
+extension UIView {
+    func bounceOut(duration: TimeInterval) {
+        UIView.animate(withDuration: duration, delay: 0, usingSpringWithDamping: 0.6, initialSpringVelocity: 0.5) {
+            self.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
+        }
+    }
+}
+
+extension Int {
+    func times(_ closure: () -> Void) {
+        for _ in 0..<self {
+            closure()
+        }
+    }
+}
+
+5.times {
+    print("Hello!")
+}
+
+extension Array where Element: Comparable {
+    mutating func remove(item: Element) {
+        if self.filter({$0 == item}).count > 1 {
+            self.remove(at: self.firstIndex(of: item)!)
+        }
+    }
+}
+
+var array = [1,2,3,4,5,1]
+
+array.remove(item: 1)
+
+print(array)
